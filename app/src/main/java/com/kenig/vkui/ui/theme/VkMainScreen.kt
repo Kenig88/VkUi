856fca282cecde4a3d417aa099b1f039bc9aa28a
s.krotov <14bassdrumand88@gmail.com>
2023-08-22 15:12:51 +0300

    2 lazyColumn

--- app/src/main/java/com/kenig/vkui/ui/theme/VkMainScreen.kt
+++ app/src/main/java/com/kenig/vkui/ui/theme/VkMainScreen.kt
@@ -2,7 +2,10 @@ package com.kenig.vkui.ui.theme
 
 import PostCard
 import android.annotation.SuppressLint
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.livedata.observeAsState
@@ -12,13 +15,11 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.kenig.vkui.MainViewModel
-import com.kenig.vkui.domain.FeedPost
-import com.kenig.vkui.domain.StatisticItem
 
+@OptIn(ExperimentalMaterialApi::class, ExperimentalFoundationApi::class)
 @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @Composable
 fun MainScreen(viewModel: MainViewModel) {
-
     Scaffold(
         bottomBar = {
             BottomNavigation {
@@ -47,14 +48,44 @@ fun MainScreen(viewModel: MainViewModel) {
             }
         }
     ) {
-        val feedPost = viewModel.feedPost.observeAsState(FeedPost())
-        PostCard(
-            modifier = Modifier.padding(2.dp),
-            feedPost = feedPost.value,
-            onViewsClickListener = viewModel::updateCount,
-            onShareClickListener = viewModel::updateCount,
-            onCommentClickListener = viewModel::updateCount,
-            onLikeClickListener = viewModel::updateCount,
-        )
+        val feedPosts = viewModel.feedPosts.observeAsState(listOf())
+
+        LazyColumn(
+            contentPadding = PaddingValues(
+                bottom = 60.dp
+            )
+        ) {
+            items(
+                items = feedPosts.value,
+                key = { it.id }
+            ) { feedPost -> //ОБЯЗАТЕЛЬНО НУЖЕН КЛЮЧ ДЛЯ УДАЛЕНИЯ ТОЛЬКО ОДНОГО ЭЛЕМЕНТА
+                val dismissState = rememberDismissState()
+                if (dismissState.isDismissed(DismissDirection.EndToStart)) {
+                    viewModel.remove(feedPost)
+                }
+                SwipeToDismiss(
+                    modifier = Modifier.animateItemPlacement(),
+                    state = dismissState,
+                    background = {},
+                    directions = setOf(DismissDirection.EndToStart)
+                ) {
+                    PostCard(
+                        feedPost = feedPost,
+                        onViewsClickListener = { statisticItem ->
+                            viewModel.updateCount(feedPost, statisticItem)
+                        },
+                        onShareClickListener = { statisticItem ->
+                            viewModel.updateCount(feedPost, statisticItem)
+                        },
+                        onCommentClickListener = { statisticItem ->
+                            viewModel.updateCount(feedPost, statisticItem)
+                        },
+                        onLikeClickListener = { statisticItem ->
+                            viewModel.updateCount(feedPost, statisticItem)
+                        }
+                    )
+                }
+            }
+        }
     }
 }
